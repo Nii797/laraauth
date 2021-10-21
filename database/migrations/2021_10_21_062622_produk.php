8d2d5f1bb95a49365f85abb761b6cbc6ed00b7bb
Nii797 <Phanterasonpardus797@gmail.com>
2021-10-21 13:39:51 +0700

    add model and modification eksperiment example crud

---
+++ database/migrations/2021_10_21_062622_produk.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Produk extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produk', function (Blueprint $table){
+            $table->id();
+            $table->string("namaproduk");
+            $table->string("deskripsi");
+            $table->string("gambar");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
