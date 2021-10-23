c0f002d855d2d4cb8688f5130427693a90a6a2d2
Nii797 <Phanterasonpardus797@gmail.com>
2021-10-23 23:55:35 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -3,23 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes(['verify' => true]);
-
 Route::get('/home', 'HomeController@index')->name('home');
-
 Route::view('watch-the-movie', 'movie')->middleware('verified');
